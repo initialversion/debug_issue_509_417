6a359e58de0db2a28d18425d550b259da0866d3c
Murugan <murugu2012@gmail.com>
2017-01-02 20:22:07 +0530

    Whitelist columns in ClassMember for ActiveAdmin

--- app/admin/class_member.rb
+++ app/admin/class_member.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ClassMember do
 
+ permit_params :student_id, :course_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
