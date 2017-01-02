f89bae308cc4060a7f3db24c1c39c3e04c12e7f5
Murugan <murugu2012@gmail.com>
2017-01-02 20:21:34 +0530

    Direct association between class_comments and reviews

--- app/models/class_comment.rb
+++ app/models/class_comment.rb
@@ -1,6 +1,10 @@
 class ClassComment < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "session_comment_id",
+             :dependent => :destroy
+
   belongs_to :class_member
 
   # Indirect associations
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :class_comment,
+             :foreign_key => "session_comment_id"
+
   belongs_to :class_member
 
   # Indirect associations
