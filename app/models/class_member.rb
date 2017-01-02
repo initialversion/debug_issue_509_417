ed521693430e20b9785ba1c752847271c5d4f0f6
Murugan <murugu2012@gmail.com>
2017-01-02 20:21:34 +0530

    Direct association between students and class_members

--- app/models/class_member.rb
+++ app/models/class_member.rb
@@ -1,6 +1,8 @@
 class ClassMember < ApplicationRecord
   # Direct associations
 
+  belongs_to :student
+
   # Indirect associations
 
   # Validations
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
 class Student < ApplicationRecord
   # Direct associations
 
+  has_many   :class_members,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
