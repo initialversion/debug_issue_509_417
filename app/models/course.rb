766aa574cb93bdb7f97322d80e457fe819a0fe8c
Murugan <murugu2012@gmail.com>
2017-01-02 20:21:34 +0530

    Direct association between professors and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ApplicationRecord
   # Direct associations
 
+  belongs_to :professor
+
   has_many   :class_members,
              :dependent => :destroy
 
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,6 +1,10 @@
 class Professor < ApplicationRecord
   # Direct associations
 
+  has_many   :classes,
+             :class_name => "Course",
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
