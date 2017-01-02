dc032127a17f89aad4a305f0ccc1cb35b61ec587
Murugan <murugu2012@gmail.com>
2017-01-02 20:21:55 +0530

    Whitelist columns in Student for ActiveAdmin

--- app/admin/student.rb
+++ app/admin/student.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Student do
 
+ permit_params :name, :last_name, :mba_class, :photo, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
